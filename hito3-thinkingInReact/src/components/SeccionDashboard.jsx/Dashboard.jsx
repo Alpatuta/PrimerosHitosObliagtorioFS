1edcda2a4b5915c0c54f02ae9b749eb81f0b643c
foteiza <fede.oteiza@gmail.com>
2026-05-22 12:35:41 -0300

    REF: Mover componente Dashboard a nueva ubicación en SeccionDashboard.jsx. Comentamos estructura de componentes en el HTML

---
+++ hito3-thinkingInReact/src/components/SeccionDashboard.jsx/Dashboard.jsx
@@ -0,0 +1,13 @@
+const Dashboard = () => {
+  return (
+    <section
+      id="dashboard"
+      className="screen screen-dashboard"
+      aria-labelledby="dashboard-title"
+    >
+      <div className="app-layout"></div>
+    </section>
+  );
+};
+
+export default Dashboard;
